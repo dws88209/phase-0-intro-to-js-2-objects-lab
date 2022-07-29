2cba1e18022e50620f6cdb49c227874729f5a0b3
Destinee Saunders <dsaunders290@gmail.com>
2022-07-28 21:39:44 -0400

    commit tree e7832e08cd3d6fcd593cfeeb60e77507c493d03f

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee ={
+    name: "Destinee", 
+streetAddress: "123 Main"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+ const copyOfemployee = {...employee}
+ copyOfemployee[key] = value
+ return copyOfemployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+   employee[key] = value
+   return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const copyOfemployee ={...employee}
+    delete copyOfemployee[key]
+    return copyOfemployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
